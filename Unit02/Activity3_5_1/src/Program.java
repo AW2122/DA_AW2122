a2b674274c152a151408fb17788bb267763b0354
Alex <57219372+AW2122@users.noreply.github.com>
2021-09-30 16:05:25 +0200

    30/09

--- Unit02/Activity3_5_1/src/Program.java
+++ Unit02/Activity3_5_1/src/Program.java
@@ -2,14 +2,20 @@ import java.io.FileInputStream;
 
 public class Program {
     public static void main(String[] args) {
-        byte[] buffer = new byte[4];
+        byte[] buffer = new byte[6];
         try {
             FileInputStream image = new FileInputStream("D:\\\\Wallpapers\\4PdYNA3.jpeg");
-            image.read(buffer, 0, 4);
-            int i = 0;
-            for (byte b:buffer) {
-                if (( & 0xFF))
+            image.read(buffer);
+            if ((buffer[0] & 0xFF) == 0xFF && (buffer[1] & 0xFF) == 0xD8 && (buffer[2] & 0xFF) == 0xFF && (buffer[3] & 0xFF) == 0xE0) {
+                System.out.println("The image is a .JPEG");
             }
+            if ((buffer[0] & 0xFF) == 0x42 && (buffer[1] & 0xFF) == 0x4D) {
+                System.out.println("The image is a .BMP");
+            }
+            if ((buffer[0] & 0xFF) == 0x47 && (buffer[1] & 0xFF) == 0x49 && (buffer[2] & 0xFF) == 0x46 && (buffer[3] & 0xFF) == 0x46) {
+                System.out.println("The image is a .GIF");
+            }
+
         }
         catch (Exception e){
 
