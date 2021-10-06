6b446f515a48c64002d28f7ff6c8f29a26039722
Alex <57219372+AW2122@users.noreply.github.com>
2021-10-06 20:45:44 +0200

    06/10

--- Unit02/Activity4_4_1/src/Program.java
+++ Unit02/Activity4_4_1/src/Program.java
@@ -1,25 +1,32 @@
 import java.io.*;
-import java.util.Locale;
 import java.util.Scanner;
 
 public class Program {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        PrintWriter printWriter  = null;
+        boolean overwrite = false;
         try {
             System.out.printf("Please enter the name of the file: ");
-            PrintWriter printWriter  = null;
-            boolean overwrite = false;
             String file = sc.nextLine();
-            String input = sc.nextLine();
-
+            String input;
 
             if (new File(file).exists()) {
-                System.out.println("The file exists. Would you like to overwrite it? Y/N ");
                 printWriter = new PrintWriter(new BufferedWriter(new FileWriter(file, overwrite)));
-                input = sc.nextLine();
+                System.out.println("The file exists. Would you like to overwrite it? Y/N ");
+                do {
+                    input = sc.nextLine();
+                } while (!input.matches("(?i)[YN]"));
+
                 if (input.equalsIgnoreCase("y")) {
                     overwrite = true;
-
+                    System.out.println("Type sentences to add to the file. Type \"exit\" + press Enter to finish. ");
+                    do {
+                        input = sc.nextLine();
+                        if (!input.equalsIgnoreCase("exit")) {
+                            printWriter.println(input);
+                        }
+                    } while (!input.equalsIgnoreCase("exit"));
 
                 } else {
                     overwrite = false;
@@ -27,11 +34,16 @@ public class Program {
                 }
             }
             else {
-                System.out.println("File does not exist");
+                System.out.println("File does not exist. Creating file...");
             }
         }
         catch (Exception e) {
 
+        }
+        finally {
+            if (printWriter != null) {
+                printWriter.close();
+            }
         }
 
     }
