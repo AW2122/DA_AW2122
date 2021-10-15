425c7a86c937433d58e7e4354a976661785f0759
Alex <57219372+AW2122@users.noreply.github.com>
2021-10-15 18:37:28 +0200

    15/10

--- Unit02/Activity7_2_1/src/Program.java
+++ Unit02/Activity7_2_1/src/Program.java
@@ -3,11 +3,15 @@ import org.xml.sax.SAXException;
 import org.xml.sax.helpers.DefaultHandler;
 import javax.xml.parsers.SAXParser;
 import javax.xml.parsers.SAXParserFactory;
+import java.util.ArrayList;
 
 
 class myXMLContactsHandler extends DefaultHandler {
     protected String currentTag;
     protected StringBuilder tagContent = new StringBuilder();
+    protected boolean inPhone = false;
+    protected String name;
+    protected String homePhone;
 
     // Tag opening found
     //
@@ -17,25 +21,35 @@ class myXMLContactsHandler extends DefaultHandler {
         if (currentTag.equals("contact")) {
             System.out.println("ID: " + attributes.getValue("id"));
         }
+        if (currentTag.equals("phones")) {
+            inPhone = true;
+        }
     }
 
     // Tag content, usually CDATA
     //
     public void characters(char[] ch, int start, int length) throws SAXException {
-            tagContent.append(ch, start, length);
+        tagContent.append(ch, start, length);
     }
 
     // Tag ending
     //
     public void endElement(String uri, String localName, String qName) throws SAXException {
         if (!currentTag.isBlank()) {
-            if (qName.equals("name")) {
-                System.out.println(" " + qName + ": " + tagContent.toString());
+            if (qName.equals("name"))
+                name = tagContent.toString();
+            if (qName.equals("surname"))
+                System.out.println(" " + "Full name: " + name + " " + tagContent.toString());
+            if (inPhone) {
+                if (qName.equals("cell"))
+                    System.out.println(" " + currentTag + " " + tagContent.toString());
+                if (qName.equals("home"))
+                    System.out.println(" " + currentTag + " " + tagContent.toString());
+                if (qName.equals("work"))
+                    System.out.println(" " + currentTag + " " + tagContent.toString());
             }
-            if (qName.equals("cell")) {
-                System.out.println(" " + qName + ": " + tagContent.toString());
-            }
-
+            if (qName.equals("phones"))
+                inPhone = false;
         }
     }
 }
