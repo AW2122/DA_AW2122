bbbaefa136e951c446d364aa1a1fdd83138881a4
Alex <57219372+AW2122@users.noreply.github.com>
2021-11-06 22:19:40 +0100

    06/11

---
+++ Unit03/FinalActivity/src/main/java/org/example/Database.java
@@ -0,0 +1,65 @@
+package org.example;
+
+import java.sql.*;
+import java.util.ArrayList;
+
+public class Database {
+    static final String url = "jdbc:postgresql://localhost:5432/VTInstitue";
+    static final String user = "postgres";
+    static final String pwd = "0023";
+    PreparedStatement pstmt = null;
+
+    public ArrayList<String> getData(String table, int index, String condition, String column) {
+        ArrayList<String> idList = new ArrayList<>();
+        try {
+            Connection conn = DriverManager.getConnection(url, user, pwd);
+            if (condition.equals(""))
+                pstmt = conn.prepareStatement("SELECT * FROM " + table);
+            else {
+                pstmt = conn.prepareStatement("SELECT * FROM " + table + " WHERE " + column + " = ?");
+                pstmt.setString(1, condition);
+            }
+            ResultSet rs = pstmt.executeQuery();
+            while (rs.next()) {
+                idList.add(rs.getString(index));
+            }
+            return idList;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return idList;
+    }
+
+    public int addStudent(Student student) {
+        try {
+            Connection conn = DriverManager.getConnection(url, user, pwd);
+            pstmt = conn.prepareStatement("INSERT INTO student (firstname, lastname, idcard, email, phone) VALUES (?, ?, ?, ?, ?)");
+            pstmt.setString(1, student.getFirstName());
+            pstmt.setString(2, student.getLastName());
+            pstmt.setString(3, student.getIdCard());
+            pstmt.setString(4, student.getEmail());
+            pstmt.setString(5, student.getPhone());
+            pstmt.executeUpdate();
+            return 0;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return 1;
+        }
+    }
+
+    public int enrollStudent(String studentId, int courseCode) {
+        try {
+            Connection conn = DriverManager.getConnection(url, user, pwd);
+            pstmt = conn.prepareStatement("INSERT INTO enrollment (student, course) VALUES (?, ?)");
+            pstmt.setString(1, studentId);
+            pstmt.setInt(2, courseCode);
+            pstmt.executeUpdate();
+            return 0;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return 1;
+        }
+    }
+
+
+}
--- Unit03/FinalActivity/src/main/java/org/example/UserInterface.java
+++ Unit03/FinalActivity/src/main/java/org/example/UserInterface.java
@@ -1,6 +1,8 @@
 package org.example;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class UserInterface {
     private JTabbedPane tabbedPane1;
@@ -14,14 +16,14 @@ public class UserInterface {
     private JLabel lblIdCard;
     private JLabel lblEmail;
     private JLabel lblPhone;
-    private JTextField textField1;
-    private JTextField textField2;
-    private JTextField textField3;
-    private JTextField textField4;
-    private JTextField textField5;
+    private JTextField tfFirstName;
+    private JTextField tfLastName;
+    private JTextField tfIdCard;
+    private JTextField tfMail;
+    private JTextField tfPhone;
     private JButton btnAdd;
-    private JComboBox comboBox1;
-    private JComboBox comboBox2;
+    private JComboBox cbStudent;
+    private JComboBox cbCourse;
     private JButton btnEnroll;
     private JLabel lblStudentE;
     private JLabel lblCourse;
@@ -30,6 +32,43 @@ public class UserInterface {
     private JButton btnPrint;
     private JLabel lblStudentR;
     private JButton btnImport;
+    private JLabel lblStatus;
+    private Database db = new Database();
+
+    public UserInterface() {
+        UpdateCombo();
+        btnAdd.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (tfFirstName.getText().isBlank() || tfLastName.getText().isBlank() || tfIdCard.getText().isBlank() || tfMail.getText().isBlank() || tfPhone.getText().isBlank())
+                    lblStatus.setText("Some fields are missing.");
+                else {
+                    Student student = new Student(tfFirstName.getText(), tfLastName.getText(), tfIdCard.getText(), tfMail.getText(), tfPhone.getText());
+                    if (db.addStudent(student) == 1)
+                        lblStatus.setText("The student already exists.");
+                    else
+                        lblStatus.setText("Student added correctly.");
+                }
+
+            }
+        });
+        btnEnroll.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String courseName = cbCourse.getSelectedItem().toString();
+                String courseCode = db.getData("courses", 1, courseName, "name").get(0);
+                db.enrollStudent(cbStudent.getSelectedItem().toString(), Integer.parseInt(courseCode));
+            }
+        });
+    }
+    public void UpdateCombo() {
+        for (String StudentId: db.getData("student", 3, "","idCard")) {
+            cbStudent.addItem(StudentId);
+        }
+        for (String courseId: db.getData("courses", 2, "", "*")) {
+            cbCourse.addItem(courseId);
+        }
+    }
 
     public static void main(String[] args) {
         JFrame frame = new JFrame("UserInterface");
